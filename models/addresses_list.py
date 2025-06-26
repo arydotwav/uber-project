93bf38482455e3aa03e9329368060dd520afb248
arydotwav <arianarz268@gmail.com>
2025-06-26 19:35:58 -0300

    integrated-addresslist-w-tripui

---
+++ models/addresses_list.py
@@ -0,0 +1,27 @@
+class AddressList:
+    def __init__(self):
+        self.list = [
+            "emperanza 1061"
+        ]
+
+    def check_address(self, address):
+        if address in self.list:
+            return f"address {address}  in list. selected for trip.\n"
+        else:
+            print(f"{address} not in saved list.")
+            option = int(input("would you like to add this address to your saved list?\n1 - YES\n2 - NO\nOption: "))
+            if option == 1:
+                self.list.append(address)
+                return f"address {address} saved. selected for trip."
+            else:
+                return(f"{address} not saved. still selected for trip")
+           
+        
+        
+    def __str__(self):
+        return f"YOUR ADDRESSES: {', '.join(self.list)}"
+        
+    
+# address = AddressList()
+# print(address.check_address("emperanza 1061"))
+# print(address)
--- models/trip.py
+++ models/trip.py
@@ -1,1 +1,33 @@
+import random
+from addresses_list import AddressList 
+class TripUI:
+    def __init__(self, pickup, dropoff, address_book):
+        self.pickup = pickup
+        self.dropoff = dropoff
+        self.address_book = address_book
 
+    def __str__(self):
+        return f"pickup: {self.pickup} # droppoff: {self.dropoff}"
+
+    def price(self):
+        km = random.randint(1, 1000)
+        #si le toca muchos km le va a salir más caro, suerte o suerte :P
+
+        if km < 300:
+            #el precio base del viaje es 20, se le suma el precio por km
+            price = 20 + (km * 6.50)
+            return f"price of trip: ${price}"
+        elif km > 300:
+            price = 20 + (km * 10.50)
+            return f"price of trip: ${price}"
+    
+    def validate_addresses(self):
+        print(self.address_book.check_address(self.pickup))
+        print(self.address_book.check_address(self.dropoff))
+
+# address_book = AddressList()
+# trip1 = TripUI("emperanza 1061", "los pozos 1020", address_book)
+# trip1.validate_addresses()
+
+# print(trip1.price())
+# print(trip1)
