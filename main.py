7d9f1eb1f4196be89895e27a2b3c99f88861832c
arydotwav <arianarz268@gmail.com>
2025-07-08 15:25:15 -0300

    base-for-mainfile

--- main.py
+++ main.py
@@ -1,42 +1,7 @@
-from auth.auth_manager import AuthManager
-from auth.user import Passanger, Driver
-from core.controller import Controller
-
-class TripUI:
-    def __init__(self):
-        self.user = None
-
-    def start_trip(self, user):
-        self.user = user
-        print(f"🚗 Starting trip for {self.user.name} ({self.user.email})")
+from ui.trip_ui import TripUi
 
 def main():
-    auth = AuthManager()
-    trip_ui = TripUI()
-    controller = Controller()
-
-    username = input("Username: ")
-    password = input("Phone (used as password): ")
-
-    if auth.login(username, password):
-        user = auth.get_current_user()
-        if isinstance(user, Passanger):
-            trip_ui.start_trip(user)
-            controller.confirm_trip()
-        elif isinstance(user, Driver):
-            show = input(("(Driver)Do you want to show as active?\n(y/n)"))
-            if show == "y":
-                controller = Controller()
-                user.status = True
-                controller.find_trip()
-            else:
-                print("You will stay as inactive.")
-        
-        option = input("Do you want to logout? (y/n): ").lower()
-        if option == 'y':
-            auth.logout()
-    else:
-        print("Access denied.")
+   ui = TripUi()
 
 if __name__ == "__main__":
     main()
--- ui/trip_ui.py
+++ ui/trip_ui.py
@@ -6,14 +6,38 @@ import random
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from core.trip_manager import TripManager
-
 from ui.follow_up import FollowUp
+from auth.user import Driver, Passanger
+from core.controller import Controller
+from auth.auth_manager import AuthManager
 
-from test.user import Driver
+class TripUi(Controller):
 
-#Usar las demas clases para obtener los datos
+    def login(self):
+        auth = AuthManager()
+        controller = Controller()
 
-class TripUi():
+        username = input("Username: ")
+        password = input("Phone (used as password): ")
+
+        if auth.login(username, password):
+            user = auth.get_current_user()
+            if isinstance(user, Passanger):
+                controller.confirm_trip()
+            elif isinstance(user, Driver):
+                show = input(("(Driver)Do you want to show as active?\n(y/n)"))
+                if show == "y":
+                    controller = Controller()
+                    user.status = True
+                    controller.find_trip()
+                else:
+                    print("You will stay as inactive.")
+            
+            option = input("Do you want to logout? (y/n): ").lower()
+            if option == 'y':
+                auth.logout()
+        else:
+            print("Access denied.")
 
     def __init__(self):
         self.logged_in= True
@@ -33,7 +57,7 @@ class TripUi():
 
         return f"Conductores disponibles: {self.active_driver}"
     
-    def trip_confirmed():
+    def trip_confirmed(self):
         print("To do")
 
     def show_driver_info(self):
@@ -49,18 +73,12 @@ class TripUi():
         """
     
     def follow_up(self):
-
         self.follow_up.show_info()
 
-
+    def start_trip(self):
+        self.login()
 
 trip = TripUi()
 
 #print(trip.welcome())
-
-print(trip.show_drivers())
-
-print(trip.show_driver_info())
-
-print(trip.follow_up())
-
+trip.start_trip()
